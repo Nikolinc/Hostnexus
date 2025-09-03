ae0abc289720fd3cc1cb25873b2d2d20a52afc68
Nikita Teigulsky <tsigulskynikita@MacBook-Air-Nikita.local>
2025-09-03 19:13:52 +0400

    backend init

--- frontend/src/pages/Servers.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Servers() {
-  return (
-    <div>Servers</div>
-  )
-}
-
-export default Servers
--- frontend/src/routes/AppRouter.tsx
+++
@@ -1,8 +0,0 @@
-import Login from "../pages/Login";
-
-
-const AppRouter = () => {
-  return <Login />;
-};
-
-export default AppRouter;
